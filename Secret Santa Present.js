114a2b690942a39c63c91855d387b582df44f871
Camto <www.ben.pr@gmail.com>
2020-12-14 15:26:22 -0400

    Capitalise "V" and start golfing.

--- Secret Santa Present.js
+++ Secret Santa Present.js
@@ -1,56 +1,10 @@
+map_range = (v,af,bf,at,bt) => ((v-af)/(bf-af))*(bt-at)+at
+
+function hue(c) {c%=360;var r,g,b;if(c<=60||c>=300)r=255;if(c>=60&&c<=180)g=255;if(c>=180&&c<=300)b=255;if(c>=60&&c<=120)r=map_range(c,60,120,255,0);else if(c<=300&&c>=240)r=map_range(c,240,300,0,255);if(c>=0&&c<=60)g=map_range(c,0,60,0,255);else if(c>=180&&c<=240)g=map_range(c,180,240,255,0);if(c>=120&&c<=180)b=map_range(c,120,180,0,255);else if(c>=300&&c<=360)b=map_range(c,300,360,255,0);return[r,g,b].map(Math.round)}
+
+gen = b => (function*(){}).constructor("var t=[0,0,0,0];var p=0;"+b.replace(/[+\-<>.[\]]/g, c=>"t[p]++;t[p]--;p--;p++;yield t[p];while(t[p]){;}".split(";")["+-<>.[]".indexOf(c)]+";"))()
+
 setTimeout(() => {
-	function map_x_to_y(val, min1, max1, min2, max2) {
-		return (((val - min1) / (max1 - min1)) * (max2 - min2)) + min2;
-	}
-	
-	function hue(given) {
-		colour = given;
-		
-		for(var count = 0; count < 100; count++) {
-			if(colour > 360) {
-				colour -= 360;
-			} else if(colour < 0) {
-				colour += 360;
-			} else {
-				break;
-			}
-		}
-		
-		var red = green = blue = 0;
-		if(colour <= 60 || colour >= 300) {
-			red = 255;
-		}
-		if(colour >= 60 && colour <= 180) {
-			green = 255;
-		}
-		if(colour >= 180 && colour <= 300) {
-			blue = 255;
-		}
-		
-		if(colour >= 60 && colour <= 120) {
-			red = map_x_to_y(colour, 60, 120, 255, 0);
-		} else if(colour <= 300 && colour >= 240) {
-			red = map_x_to_y(colour, 240, 300, 0, 255);
-		}
-		if(colour >= 0 && colour <= 60) {
-			green = map_x_to_y(colour, 0, 60, 0, 255);
-		} else if(colour >= 180 && colour <= 240) {
-			green = map_x_to_y(colour, 180, 240, 255, 0);
-		}
-		
-		if(colour >= 120 && colour <= 180) {
-			blue = map_x_to_y(colour, 120, 180, 0, 255);
-		} else if(colour >= 300 && colour <= 360) {
-			blue = map_x_to_y(colour, 300, 360, 255, 0);
-		}
-		
-		return [Math.round(red), Math.round(green), Math.round(blue)];
-	}
-	
-	function run_bf(bf) {
-		return (function*(){}).constructor("var t=Array(10).fill(0);var p=0;"+bf.replace(/[+-<>.[\]]/g, c=>"t[p]++;t[p]--;p--;p++;yield t[p];while(t[p]){;}".split(";")["+-<>.[]".indexOf(c)]+";"))()
-	}
-	
 	screen = document.createElement("canvas");
 	screen.width = 608;
 	screen.height = 160;
@@ -65,23 +19,25 @@ setTimeout(() => {
 	FLand.data.fill(255);
 	
 	c = 0;
-	poss = [3, 0, 3, 2, 3, 3, 3, 4, 3, 5, 3, 6, 3, 7, 3, 8, 2, 9, 1, 9, 0, 8];
-	g=run_bf(`
+	g=gen(`
+		// j
 		+++.>.+>+++++++[-<<.>+.>]<<-.>+.<-.>.<-.>-.<
+		// p
 		+++++.>+.>+++++++[-<<.>-.>]+++[-<<+.>.>]<<+>>++++[-<<.>+.>]<+>+++[-<<-.>.>]<<
-		+++++>----->+++[-<<.>.<.>+.<+>+>]<--->++[-<<.>.<.>-.<+>->]<<
-		+.>-.+>++++++[-<<.>+.>]<<
+		// V
+		+++++>-------->+++[-<<.>+.>]<<+>>+++[-<<.>+.>]<<+.>+.<.>+.<+>->+++[-<<.>-.>]<<+>>+++[-<<.>-.>]<<
+		// i
+		++.>.+>++++++[-<<.>+.>]<<
+		// nn
 		>>>++[-<<< ++>------>++++++[-<<.>+.>]<<+.>----.<+.>-.<+.>.<+>>+++++[-<<.>+.>] >]<<<
+		// i
 		++.>-------.+>++++++[-<<.>+.>]<<
+		// e
 		++++++.>-.+>+++[-<<-.>.>]<<->>++++[-<<.>-.>]<->+++[-<<+.>.>]<<+.>+.+>++++[-<<.->.>]
 	`);
-	d = Array(10).fill(0).map(()=>Array(38).fill(false));
-	for(var i = 0; i < poss.length; i+=2) {
-		//d[poss[i+1]][poss[i]] = true;
-	}
-	//while(!(a=g.next()).done) {console.log(a.value,b=g.next().value);d[b][a.value]=true}
+	d = Array(10).fill(0).map(()=>Array(38).fill(""));
 	
-	t = 80;
+	t = 72;
 	
 	setInterval(() => {
 		t++;
@@ -96,8 +52,7 @@ setTimeout(() => {
 		}
 		
 		if(!(x=g.next()).done) {
-			y=g.next()
-			console.log(x.value, y.value); d[y.value][x.value] = true;
+			d[g.next().value][x.value] = [];
 		}
 		ctx.putImageData(FLand, 0, 0);
 	}, 1000/30);
