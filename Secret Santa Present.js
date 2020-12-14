6ab3395163b57e32aeaff5410dc93aea679ca7e4
Camto <www.ben.pr@gmail.com>
2020-12-14 18:42:50 -0400

    Add border around letters and lower fps.

--- Secret Santa Present.js
+++ Secret Santa Present.js
@@ -2,7 +2,7 @@ function hue(c) {m=(v,af,bf,at,bt)=>((v-af)/(bf-af))*(bt-at)+at;var r,g,b;c%=360
 
 gen = (function*(){}).constructor("var t=[0,0,0,0];var p=0;"+`
 // j
-+++.>.+>+++++++[-<<.>+.>]<<-.>+.<-.>.<-.>-.<
+++++.>+.+>+++++++[-<<.>+.>]<<-.>+.<-.>.<-.>-.<
 // p
 +++++.>+.>+++++++[-<<.>-.>]+++[-<<+.>.>]<<+>>++++[-<<.>+.>]<+>+++[-<<-.>.>]<<
 // V
@@ -18,4 +18,4 @@ gen = (function*(){}).constructor("var t=[0,0,0,0];var p=0;"+`
 `.replace(/[+\-<>.[\]]/g, c=>"t[p]++;t[p]--;p--;p++;yield t[p];while(t[p]){;}".split(";")["+-<>.[]".indexOf(c)]+";"))()
 
 //main
-setTimeout(() => {s=document.createElement("canvas");y=s.style;s.width=608;s.height=160;p=s.getContext("2d");document.body.appendChild(s);y.position="absolute";y.margin="auto";y.top=y.left=y.bottom=y.right=0;i=p.createImageData(s.width,s.height);i.data.fill(255);c=0;d=Array(10).fill(NaN).map(n=>Array(38).fill("".slice(n)));t=72;setInterval(m=>{t++;m++;for(y=0;y<s.height;y++)for(x=~0;x<s.width+~[];)if(d[Math.floor(y/16)][Math.floor(++x/16)])for(n=0;n<3;n++)i.data[x*4+y*s.width*4+n]=hue(t)[n];if(!(x=gen.next()).done)d[gen.next().value][x.value]=[];p.putImageData(i,0,0)},1000/30)},10);
+setTimeout(() => {s=document.createElement("canvas");y=s.style;s.width=640;s.height=192;p=s.getContext("2d");document.body.appendChild(s);y.position="absolute";y.margin="auto";y.top=y.left=y.bottom=y.right=0;i=p.createImageData(s.width,s.height);i.data.fill(255);c=0;d=Array(12).fill(NaN).map(n=>Array(40).fill("".slice(n)));t=72;setInterval(m=>{t++;m++;for(y=0;y<s.height;y++)for(x=~0;x<s.width+~[];)if(d[Math.floor(y/16)][Math.floor(++x/16)])for(n=~0;n<2;)i.data[x*4+y*s.width*4+ ++n]=d[Math.floor(y/16)][Math.floor(x/16)]==2?hue(t)[n]:hue(t+180)[n];if(!(x=gen.next()).done){y=gen.next().value;d[y][x.value]=-~-~"";for(k=-1;k<2;k++)for(j=-1;j<2;)d[y+k][x.value+j++]||={}};p.putImageData(i,0,0)},1000/10)},10);
