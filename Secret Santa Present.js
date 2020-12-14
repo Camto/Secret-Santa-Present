507d0e87cff0f4ea7bb48b925bcdc407c2652906
Camto <www.ben.pr@gmail.com>
2020-12-14 15:45:26 -0400

    Golf nicely.

--- Secret Santa Present.js
+++ Secret Santa Present.js
@@ -2,58 +2,22 @@ map_range = (v,af,bf,at,bt) => ((v-af)/(bf-af))*(bt-at)+at
 
 function hue(c) {c%=360;var r,g,b;if(c<=60||c>=300)r=255;if(c>=60&&c<=180)g=255;if(c>=180&&c<=300)b=255;if(c>=60&&c<=120)r=map_range(c,60,120,255,0);else if(c<=300&&c>=240)r=map_range(c,240,300,0,255);if(c>=0&&c<=60)g=map_range(c,0,60,0,255);else if(c>=180&&c<=240)g=map_range(c,180,240,255,0);if(c>=120&&c<=180)b=map_range(c,120,180,0,255);else if(c>=300&&c<=360)b=map_range(c,300,360,255,0);return[r,g,b].map(Math.round)}
 
-gen = b => (function*(){}).constructor("var t=[0,0,0,0];var p=0;"+b.replace(/[+\-<>.[\]]/g, c=>"t[p]++;t[p]--;p--;p++;yield t[p];while(t[p]){;}".split(";")["+-<>.[]".indexOf(c)]+";"))()
+gen = (function*(){}).constructor("var t=[0,0,0,0];var p=0;"+`
+// j
++++.>.+>+++++++[-<<.>+.>]<<-.>+.<-.>.<-.>-.<
+// p
++++++.>+.>+++++++[-<<.>-.>]+++[-<<+.>.>]<<+>>++++[-<<.>+.>]<+>+++[-<<-.>.>]<<
+// V
++++++>-------->+++[-<<.>+.>]<<+>>+++[-<<.>+.>]<<+.>+.<.>+.<+>->+++[-<<.>-.>]<<+>>+++[-<<.>-.>]<<
+// i
+++.>.+>++++++[-<<.>+.>]<<
+// nn
+>>>++[-<<< ++>------>++++++[-<<.>+.>]<<+.>----.<+.>-.<+.>.<+>>+++++[-<<.>+.>] >]<<<
+// i
+++.>-------.+>++++++[-<<.>+.>]<<
+// e
+++++++.>-.+>+++[-<<-.>.>]<<->>++++[-<<.>-.>]<->+++[-<<+.>.>]<<+.>+.+>++++[-<<.->.>]
+`.replace(/[+\-<>.[\]]/g, c=>"t[p]++;t[p]--;p--;p++;yield t[p];while(t[p]){;}".split(";")["+-<>.[]".indexOf(c)]+";"))()
 
-setTimeout(() => {
-	screen = document.createElement("canvas");
-	screen.width = 608;
-	screen.height = 160;
-	ctx = screen.getContext("2d");
-	document.body.appendChild(screen);
-	screen.style.position = "absolute";
-	screen.style.margin = "auto";
-	screen.style.top = screen.style.left = screen.style.bottom = screen.style.right = "0px";
-	
-	FLand = ctx.createImageData(screen.width, screen.height);
-	
-	FLand.data.fill(255);
-	
-	c = 0;
-	g=gen(`
-		// j
-		+++.>.+>+++++++[-<<.>+.>]<<-.>+.<-.>.<-.>-.<
-		// p
-		+++++.>+.>+++++++[-<<.>-.>]+++[-<<+.>.>]<<+>>++++[-<<.>+.>]<+>+++[-<<-.>.>]<<
-		// V
-		+++++>-------->+++[-<<.>+.>]<<+>>+++[-<<.>+.>]<<+.>+.<.>+.<+>->+++[-<<.>-.>]<<+>>+++[-<<.>-.>]<<
-		// i
-		++.>.+>++++++[-<<.>+.>]<<
-		// nn
-		>>>++[-<<< ++>------>++++++[-<<.>+.>]<<+.>----.<+.>-.<+.>.<+>>+++++[-<<.>+.>] >]<<<
-		// i
-		++.>-------.+>++++++[-<<.>+.>]<<
-		// e
-		++++++.>-.+>+++[-<<-.>.>]<<->>++++[-<<.>-.>]<->+++[-<<+.>.>]<<+.>+.+>++++[-<<.->.>]
-	`);
-	d = Array(10).fill(0).map(()=>Array(38).fill(""));
-	
-	t = 72;
-	
-	setInterval(() => {
-		t++;
-		for(var y = 0; y < screen.height; y++) {
-			for(var x = 0; x < screen.width; x++) {
-				if(d[Math.floor(y/16)][Math.floor(x/16)]) {
-					for(var i = 0; i < 3; i++) {
-						FLand.data[(x * 4) + (y * screen.width * 4) + i] = hue(t)[i];
-					}
-				}
-			}
-		}
-		
-		if(!(x=g.next()).done) {
-			d[g.next().value][x.value] = [];
-		}
-		ctx.putImageData(FLand, 0, 0);
-	}, 1000/30);
-}, 10);
+//main
+setTimeout(() => {s=document.createElement("canvas");y=s.style;s.width=608;s.height=160;p=s.getContext("2d");document.body.appendChild(s);y.position="absolute";y.margin="auto";y.top=y.left=y.bottom=y.right=0;i=p.createImageData(s.width,s.height);i.data.fill(255);c=0;d=Array(10).fill(NaN).map(n=>Array(38).fill("".slice(n)));t=72;setInterval(m=>{t++;m++;for(y=0;y<s.height;y++)for(x=0;x<s.width;x++)if(d[Math.floor(y/16)][Math.floor(x/16)])for(n=0;n<3;n++)i.data[x*4+y*s.width*4+n]=hue(t)[n];if(!(x=gen.next()).done)d[gen.next().value][x.value]=[];p.putImageData(i,0,0)},1000/30)},10);
