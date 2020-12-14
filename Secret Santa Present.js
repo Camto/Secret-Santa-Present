f79742c72f44153d9265171bc8bacb113995a565
Camto <www.ben.pr@gmail.com>
2020-12-14 13:08:02 -0400

    Start rendering the pixel art.

--- Secret Santa Present.js
+++ Secret Santa Present.js
@@ -1,22 +1,4 @@
 setTimeout(() => {
-	class Canvas {
-		constructor(id, width, height) {
-			this.canvas = document.createElement("canvas");
-			this.canvas.width = this.width = width;
-			this.canvas.height = this.height = height;
-			this.draw = this.canvas.getContext("2d");
-			
-			this.canvas.style.position = "absolute";
-			this.canvas.style.margin = "auto";
-			this.canvas.style.top = "0px";
-			this.canvas.style.left = "0px";
-			this.canvas.style.bottom = "0px";
-			this.canvas.style.right = "0px";
-			
-			document.body.appendChild(this.canvas);
-		}
-	}
-	
 	function map_x_to_y(val, min1, max1, min2, max2) {
 		return (((val - min1) / (max1 - min1)) * (max2 - min2)) + min2;
 	}
@@ -65,24 +47,39 @@ setTimeout(() => {
 		return [Math.round(red), Math.round(green), Math.round(blue)];
 	}
 	
-	screen = new Canvas("FLand", 600, 400);
+	screen = document.createElement("canvas");
+	screen.width = 672;
+	screen.height = 224;
+	ctx = screen.getContext("2d");
+	document.body.appendChild(screen);
+	screen.style.position = "absolute";
+	screen.style.margin = "auto";
+	screen.style.top = screen.style.left = screen.style.bottom = screen.style.right = "0px";
 	
-	FLand = screen.draw.createImageData(screen.width, screen.height);
+	FLand = ctx.createImageData(screen.width, screen.height);
 	
-	w = FLand.width;
-	h = FLand.height;
+	FLand.data.fill(255);
 	
-	func = (x, y) => hue(((Math.abs(x - (w / 2)) + Math.abs(y - (h / 2))) / 2) / 0.875 + 170);
-	
-	for(var y = 0; y < h; y++) {
-		for(var x = 0; x < w; x++) {
-			colours = func(x, y);
-			FLand.data[(x * 4) + (y * w * 4)] = colours[0];
-			FLand.data[(x * 4) + (y * w * 4) + 1] = colours[1];
-			FLand.data[(x * 4) + (y * w * 4) + 2] = colours[2];
-			FLand.data[(x * 4) + (y * w * 4) + 3] = 255;
-		}
+	poss = [2, 4, 7, 9];
+	d = Array(14).fill(0).map(()=>Array(42).fill(false));
+	for(var i = 0; i < poss.length; i+=2) {
+		d[poss[i+1]][poss[i]] = true;
 	}
 	
-	screen.draw.putImageData(FLand, 0, 0);
+	t = 0;
+	white = "255;".repeat(3).split(";").slice(0,-1).map(i=>+i);
+	
+	setInterval(() => {
+		t++;
+		for(var y = 0; y < screen.height; y++) {
+			for(var x = 0; x < screen.width; x++) {
+				colours = d[Math.floor(y/16)][Math.floor(x/16)] ? hue(t) : white;
+				for(var i = 0; i < 3; i++) {
+					FLand.data[(x * 4) + (y * screen.width * 4) + i] = colours[i];
+				}
+			}
+		}
+		
+		ctx.putImageData(FLand, 0, 0);
+	}, 1000/15);
 }, 10);
